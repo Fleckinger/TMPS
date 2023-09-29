256d597b4710a582d2f46bc08579c63fc97bd3e5
Fleckinger <savarovvld@gmail.com>
2023-09-29 21:48:30 +0300

    Initial commit

---
+++ src/main/kotlin/com/fleckinger/tmps/TmpsApplication.kt
@@ -0,0 +1,11 @@
+package com.fleckinger.tmps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TmpsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TmpsApplication>(*args)
+}
---
+++ src/test/kotlin/com/fleckinger/tmps/TmpsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fleckinger.tmps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TmpsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

eb33f2260646c5ce2e5de667c54130ad2d74a3f4
Fleckinger <savarovvld@gmail.com>
2023-10-21 17:59:20 +0300

    Added variables for docker run

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+echo "Start deployment script."
 
 start=$(date +"%s")
+echo "Trying establish SSH connection..."
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_IP_ADDRESS} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+echo "SSH connection established."
 
-ssh -p ${SERVER_PORT} ${SERVER_USERNAME}@${SERVER_IP_ADDRESS} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
-docker pull fleckinger/tmps:latest
+docker pull ${DOCKER_CONTAINER_NAME}:latest
 
-CONTAINER_NAME=tmps
+CONTAINER_NAME=${DOCKER_CONTAINER_NAME}
 if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
     if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
         echo "Container is running -> stopping it..."
@@ -13,7 +16,9 @@ if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
     fi
 fi
 
-docker run -d --rm -p 8081:8081 --name $CONTAINER_NAME fleckinger/tmps:latest
+echo "Run docker container..."
+
+docker run --detach --name $CONTAINER_NAME -p 8082:8082 ${DOCKER_CONTAINER_NAME}:latest --SERVER_PORT=${APP_SERVER_PORT} --SPRING_DATASOURCE_URL=jdbc:postgresql://${DATABASE_URL} --SPRING_DATASOURCE_USERNAME=${DATABASE_USERNAME} --SPRING_DATASOURCE_PASSWORD=${DATABASE_PASSWORD} --TELEGRAM_BOT_NAME=${TELEGRAM_BOT_NAME} --TELEGRAM_BOT_ID=${TELEGRAM_BOT_ID} --TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN} --POSTING_DELAY=60000
 
 exit
 ENDSSH
